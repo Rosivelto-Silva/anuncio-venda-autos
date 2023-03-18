d97f2cf31eb57d0ac55ae0571edf1a4eecdffb21
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2023-03-18 12:07:32 -0300

    implementa classe de response

---
+++ src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/api/AutomovelResponse.java
@@ -0,0 +1,10 @@
+package com.wakandaacademywebcars.automoveis.automoveis.application.api;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Builder
+@Value
+public class AutomovelResponse {
+	private Long idAuto;
+}
