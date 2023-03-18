56624e4d58b2be6cf7d7391f4151256c36129b6b
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2023-03-18 12:06:25 -0300

    implementa contrato de api

--- src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/api/AutomovelAPI.java
+++ src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/api/AutomovelAPI.java
@@ -1,8 +1,16 @@
 package com.wakandaacademywebcars.automoveis.automoveis.application.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import jakarta.validation.Valid;
 
 @RequestMapping("v1/api/auto")
 public interface AutomovelAPI {
-
+	@PostMapping
+	@ResponseStatus(value = HttpStatus.CREATED)
+	AutomovelResponse postCadastraNovoAutomovel(@RequestBody @Valid  AutomovelRequest automovelRequest );
 }
