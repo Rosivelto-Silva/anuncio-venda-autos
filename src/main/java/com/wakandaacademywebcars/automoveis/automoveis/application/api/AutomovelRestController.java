749cdcd53612808cf40518ce9db35bbeed75c72e
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2023-03-18 12:11:47 -0300

    implementa camada de controller

--- src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/api/AutomovelRestController.java
+++ src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/api/AutomovelRestController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.wakandaacademywebcars.automoveis.automoveis.application.service.AutomovelService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -11,5 +12,13 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 @RestController
 public class AutomovelRestController implements AutomovelAPI {
-	private final AutomovelService autoService; 
+	private final AutomovelService autoService;
+
+	@Override
+	public AutomovelResponse postCadastraNovoAutomovel( AutomovelRequest automovelRequest) {
+		log.info("[inicia] AutomovelRestController - postCadastraNovoAutomovel");
+		AutomovelResponse auto = autoService.cadastraNovoVeiculo(automovelRequest);
+		log.info("[finaliza] AutomovelRestController - postCadastraNovoAutomovel");
+		return auto;
+	} 
 }
