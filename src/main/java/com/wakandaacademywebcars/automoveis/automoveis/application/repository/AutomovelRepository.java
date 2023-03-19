d10ec3fb9fabd8ae65ec10a001f53ed900dd2ff4
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2023-03-18 21:46:31 -0300

    implementa camada de repository

--- src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/repository/AutomovelRepository.java
+++ src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/repository/AutomovelRepository.java
@@ -1,7 +1,10 @@
 package com.wakandaacademywebcars.automoveis.automoveis.application.repository;
 
+import java.util.List;
+
 import com.wakandaacademywebcars.automoveis.automoveis.domain.Automovel;
 
 public interface AutomovelRepository {
 	Automovel salva(Automovel automovel);
+	List<Automovel> buscaTodosAutomoveis();
 }
