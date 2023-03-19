012209005751f8b84c1c4ccd7e48410d01ad2812
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2023-03-18 21:46:13 -0300

    implementa camada de servico

--- src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/service/AutomovelApplicationService.java
+++ src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/service/AutomovelApplicationService.java
@@ -1,7 +1,10 @@
 package com.wakandaacademywebcars.automoveis.automoveis.application.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.wakandaacademywebcars.automoveis.automoveis.application.api.AutomovelListResponse;
 import com.wakandaacademywebcars.automoveis.automoveis.application.api.AutomovelRequest;
 import com.wakandaacademywebcars.automoveis.automoveis.application.api.AutomovelResponse;
 import com.wakandaacademywebcars.automoveis.automoveis.application.repository.AutomovelRepository;
@@ -22,6 +25,14 @@ public class AutomovelApplicationService implements AutomovelService {
 		Automovel auto =autoRepository.salva(new Automovel(automovelRequest));
 		log.info("[finaliza] AutomovelApplicationService - cadastraNovoVeiculo");
 		return AutomovelResponse.builder().idAuto(auto.getIdAuto()).build();
+	}
+
+	@Override
+	public List<AutomovelListResponse> listaTodosAutomoveis() {
+		log.info("[inicia] AutomovelApplicationService - listaTodosAutomoveis");
+		List<Automovel> auto = autoRepository.buscaTodosAutomoveis();
+		log.info("[finaliza] AutomovelApplicationService - listaTodosAutomoveis");
+		return  AutomovelListResponse.converte(auto);
 	} 
 
 }
--- src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/service/AutomovelService.java
+++ src/main/java/com/wakandaacademywebcars/automoveis/automoveis/application/service/AutomovelService.java
@@ -1,8 +1,12 @@
 package com.wakandaacademywebcars.automoveis.automoveis.application.service;
 
+import java.util.List;
+
+import com.wakandaacademywebcars.automoveis.automoveis.application.api.AutomovelListResponse;
 import com.wakandaacademywebcars.automoveis.automoveis.application.api.AutomovelRequest;
 import com.wakandaacademywebcars.automoveis.automoveis.application.api.AutomovelResponse;
 
 public interface AutomovelService {
 	AutomovelResponse cadastraNovoVeiculo(AutomovelRequest automovelRequest);
+	List<AutomovelListResponse> listaTodosAutomoveis();
 }
